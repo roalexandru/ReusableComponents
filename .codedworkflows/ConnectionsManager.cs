dd506f6881266ef8f74c5709c4c1bba6ed60407e
Alexandru Roman <alexandru.roman@uipath.com>
2024-10-16 19:51:11 +0300

    add library

---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace Demo.ReusableComponents.SAP
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
